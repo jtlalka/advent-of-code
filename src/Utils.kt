3df65ebd1f20a562d4d1bbe7ba8a27a0e9611798
Jakub Tlalka <jakub@tlalka.net>
2021-12-02 08:08:38 +0100

    Day 2

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,21 +1,13 @@
 import java.io.File
-import java.math.BigInteger
-import java.security.MessageDigest
 
-/**
- * Reads lines of string from the given input txt file.
- */
-fun readInput(name: String): List<String> = File("src", "$name.txt")
+fun readStringInput(name: String): List<String> = readRawInput(name)
+
+fun readIntInput(name: String): List<Int> = readRawInput(name)
+    .map(String::toInt)
+
+fun readComplexInput(name: String, delimiter: String = " "): List<List<String>> = readRawInput(name)
+    .map { it.split(delimiter) }
+
+private fun readRawInput(name: String): List<String> = File("src", "$name.txt")
     .readLines()
-    .filter { it.isNotBlank() }
-
-/**
- * Reads lines of integers from the given input txt file.
- */
-fun readIntInput(name: String): List<Int> = readInput(name)
-    .map { it.toInt() }
-
-/**
- * Converts string to md5 hash.
- */
-fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+    .filter(String::isNotBlank)
